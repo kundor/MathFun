589245da5deedb237fa9d541b1ae1278d4a14323
Nicholas Matteo <kundor@kundor.org>
2019-08-30 09:01:56 -0600

    Odd/even logic to save time

--- riecamanN.cc
+++ riecamanN.cc
@@ -10,15 +10,44 @@ int main(int argc, char** argv) {
     unsigned long i = 0, p = 1, N;
     N = std::atol(argv[1]);
     primesieve::iterator it;
-    while (N) {
+    bool isodd = N % 2;
+
+    if (!isodd) {
+        if (N == 0) {
+            printf("0 steps\n");
+            return 0;
+        }
+        if (N == 2) {
+            printf("1 step, last prime 2\n");
+            return 0;
+        }
         p = it.next_prime();
         if (p > N) {
             N += p;
         } else {
             N -= p;
         }
+    }
+
+    for (;;) {
+        p = it.next_prime();
+        if (p > N) {
+            N += p;
+        } else {
+            N -= p;
+        }
+        /* N is odd after adding/subtracting one prime */
+        p = it.next_prime();
+        if (p > N) {
+            N += p;
+        } else {
+            N -= p;
+            if (!N) // this is the only case we can get N to 0
+                break;
+        }
+        /* Now N is even again */
         ++i;
-        if (!(i & 0x7FFFFFFul)) {
+        if (!(i & 0xFFFFFFFul)) {
             printf("%lu. %lu: %lu\n", i, p, N);
         }
     }
