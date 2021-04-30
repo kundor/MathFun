54207d571d217810e5c136557f53d5b2597a4fd9
Nicholas Matteo <kundor@kundor.org>
2021-04-30 10:13:30 -0600

    Minimal Josephus code for OEIS

---
+++ A343780.py
@@ -0,0 +1,16 @@
+# minimal extract of josephus-save-first-n.py for inclusion in OEIS
+
+def savesfirstn(n, q):
+    circle = list(range(2*n))
+    i = 0
+    while len(circle) > n:
+        i = (i + q - 1) % len(circle)
+        if circle[i] < n:
+            return False
+        circle.pop(i)
+    return True
+for n in range(1,22):
+    q = n + 1
+    while not savesfirstn(n, q):
+        q += 1
+    print(f'{n} {q}')
