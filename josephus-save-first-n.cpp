17211e70fd7b6573350db82dc357718cf9cd9fe6
Nicholas Matteo <kundor@kundor.org>
2021-04-30 09:10:32 -0600

    C++ version of direct simulation calculation

---
+++ josephus-save-first-n.cpp
@@ -0,0 +1,30 @@
+/* See josephus-save-first-n.py for extended description.
+ * Direct "simulation" approach: remove every qth element from a vector
+ * until only n are left. If we kill any "good guy" (1 to n), then return false. */
+
+#include <cstdio>
+#include <vector>
+#include <numeric>
+
+bool firstn(int n, int q) {
+    int i = 0, size = 2*n;
+    std::vector<int> circle(2*n);
+    std::iota(circle.begin(), circle.end(), 1); // fill 1...2n
+    while (size > n) {
+        i = (i + q - 1) % size;
+        if (circle[i] <= n)
+            return false;
+        circle.erase(circle.begin() + i);
+        --size;
+    }
+    return true;
+}
+
+int main() {
+    int n, q;
+    for (n = 1; n < 23; ++n) {
+        for (q = n + 1; !firstn(n, q); ++q);
+        std::printf("%2i: %i\n", n, q);
+    }
+    return 0;
+}
